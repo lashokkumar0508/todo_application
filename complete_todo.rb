13655fbfa50aa9016f8a019b0ef5d9f12ece6917
lashokkumar0508 <85612620+lashokkumar0508@users.noreply.github.com>
2021-09-29 21:51:22 +0530

    Remove method display ID

--- complete_todo.rb
+++ complete_todo.rb
@@ -2,7 +2,7 @@ require "./connect_db.rb"
 require "./todo.rb"
 
 connect_db!
-Todo.show_list_with_ID
+Todo.show_list
 
 puts "Which todo do you want to mark as complete? (Enter id): "
 todo_id = gets.strip.to_i
